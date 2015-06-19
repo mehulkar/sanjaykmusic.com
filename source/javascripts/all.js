81baf423f8f988dbe37850625779b5e86ffbcc48
Mehul Kar <mehul.kar@apple.com>
2015-06-18 22:12:44 -0700

    Basic website

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,12 @@
 //= require_tree .
+//= require jquery
+
+$('document').ready(function() {
+  var el = $('#title');
+
+  var width = el.width();
+  var docWidth = document.body.scrollWidth;
+
+  var leftPos = (docWidth/2) - (width/2);
+  el.css('left', leftPos);
+})
